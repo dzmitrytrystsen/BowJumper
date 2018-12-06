a850f22b3e9f5a8b02642dcce0874cb1a8e00fb2
dzmitrytrystsen <tristendmitry@gmail.com>
2018-12-06 18:44:25 +0300

    Initial commit

---
+++ Assets/Scripts/JumpButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JumpButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (Player.instance != null)
+            Player.instance.SetPower(true);
+
+        Debug.Log("Touching the Button");
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        if (Player.instance != null)
+            Player.instance.SetPower(false);
+
+
+        Debug.Log("Have relesed the Button");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    private Rigidbody2D myRigidbody2D;
+    public Animator myAnimator;
+
+    [SerializeField] private float jumpPowerX, jumpPowerY, jumpPowerXMax = 6.5f, jumpPowerYMax = 13.5f;
+    
+    private float tresholdX = 7f;
+    private float tresholdY = 14f;
+
+    private bool setPower, didJump;
+
+    void Awake()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+        SetUpInstance();
+    }
+
+    void Update()
+    {
+        SetPower();
+    }
+
+    private void SetUpInstance()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    private void SetPower()
+    {
+        if (setPower)
+        {
+            jumpPowerX += tresholdX * Time.deltaTime;
+            jumpPowerY += tresholdY * Time.deltaTime;
+
+            if (jumpPowerX > jumpPowerXMax)
+                jumpPowerX = jumpPowerXMax;
+
+            if (jumpPowerY > jumpPowerYMax)
+                jumpPowerY = jumpPowerYMax;
+        }
+    }
+
+    public void SetPower(bool setPower)
+    {
+        this.setPower = setPower;
+
+        if (!setPower)
+        {
+            Jump();
+        }
+
+        if (setPower)
+            Debug.Log("Setting the Power");
+
+        else
+            Debug.Log("Not setting the Power");
+    }
+
+    private void Jump()
+    {
+        myRigidbody2D.velocity = new Vector2(jumpPowerX, jumpPowerY);
+        myAnimator.SetTrigger("Jump");
+
+        jumpPowerX = jumpPowerY = 0f;
+    }
+}
